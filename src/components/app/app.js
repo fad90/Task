0e454edef3aafdb699c6ae7997afde3103f7d742
unknown <you@example.com>
2021-08-15 22:47:55 +0400

    start

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchPanel from "../search-panel/search-panel";
+
+const App = () => {
+  return (
+      <div>
+          <SearchPanel/>
+      </div>
+  )
+};
+
+export default App;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class SearchPanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+      alert('Sent message: ' + this.state.value);
+      event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+          placeholder="search"
+        />
+        <input type="submit" value="Add item" />
+      </form>
+    );
+  }
+}
+
+export default SearchPanel;
